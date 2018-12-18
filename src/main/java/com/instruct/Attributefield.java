90e3e0041719f1dc7fda784cae15d213e433d6fa
yangshuai <ys2756965290@qq.com>
2018-12-18 10:02:38 +0800

    修改结构

---
+++ src/main/java/com/instruct/Attributefield.java
@@ -0,0 +1,33 @@
+package com.instruct;
+
+import org.jsoup.nodes.Document;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * projectName : demo
+ *
+ * @author yangshuai
+ * @version ID : V1.0 , 2018/12/1718:47
+ */
+public interface Attributefield {
+
+    public Set<String> imgUrl (Document doc, Set < String > imgUrls, String txtFilePath, String path);
+
+    public String price (Document doc);
+
+    public List<String> colorList (Document doc, List < String > colorList);
+
+    public List<String> sizeContainer (Document doc, List < String > sizeList);
+
+    public List<String> productName (Document doc);
+
+    public String salePrice (Document doc);
+
+    public List<String> bulletPointList (Document doc, List < String > bulletPoint);
+
+    public String itemType (Document doc, StringBuilder itemType );
+
+}
